06dae5b0424c280ace1b0efd6b847db2db877bf1
DylanToporek <dylantoporek@gmail.com>
2021-09-21 09:55:29 -0400

    commit tree 015d1b10c98ee032280df6c280b694a99fe2bd29

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply;
+
+
+let random = Math.ceil(Math.random()*100);
+
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+mod;
+
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
+max;
 
 // Write your code here
